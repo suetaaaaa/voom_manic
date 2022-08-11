c844a9fc50e03a744ade5f26bb3e41bc448dbe4d
Alexey <anchous1709@mail.ru>
2022-08-11 18:24:06 +0300

    Create VoomUser model and register it in admin panel

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,9 @@
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class VoomUser(User):
+
+	def __str__(self):
+		return self.username
