cbe31cc7132f7b2d942217a4344e80e282a0e5fc
Alexey <anchous1709@mail.ru>
2022-08-14 13:06:05 +0300

    Implement Skills model in index.html

--- main/models.py
+++ main/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Skills(models.Model):
-	title = 'Навыки'
-	skills = models.TextField()
+	skill = models.TextField()
 
 	class Meta:
 		verbose_name_plural = 'Навыки'
 
 	def __str__(self):
-		return self.title
+		return self.skill
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+from main.models import Skills
+
 
 def index(request):
 
+	skills_list = Skills.objects.all()
+
 	data_index = {
-		'title': 'VOOM.MANIC'
+		'title': 'VOOM.MANIC',
+		'skills_list': skills_list
 	}
 
 	return render(request, 'main/index.html', data_index)
